3e93cb6f3de9368478df045de23814c0d34884a8
Andrea Terzani <andrea.terzani@gmail.com>
2015-03-04 20:37:57 +0100

    second tutorial

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,3 +1,18 @@
 
 var app =  angular.module('poolApp',['ngRoute']);
 
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/admin', {
+                templateUrl: 'js/view/admin.html',
+                controller: 'adminController'
+            }).
+            when('/view', {
+                templateUrl: 'js/view/list.html',
+                controller: 'poolController'
+            }).
+            otherwise({
+                redirectTo: '/view'
+            });
+    }]);
--- public/app/js/controller/PoolController.js
+++ public/app/js/controller/PoolController.js
@@ -1,17 +1,61 @@
-app.controller('poolController', function($scope,$http){
- 
-  $scope.pools = [];
+app.controller('poolController', function($scope, $http) {
 
-	 $http.get('/api/pool').
-	 success(function(data, status, headers, config) {
-		 $scope.pools = data;	 
-	 });
-
-	$scope.addVote = function(pooloptions){
- 		$http.get('/api/pooloption/addvote/'+ pooloptions.id).
-			success(function(data, status, headers, config) {
-				 pooloptions.vote++;
-			});
+	$scope.pools = [];
+	$http.get('/api/pool').
+	success(function(data, status, headers, config) {
+		$scope.pools = data;
+	});
+	$scope.addVote = function(pooloptions) {
+		$http.get('/api/pooloption/addvote/' + pooloptions.id).
+		success(function(data, status, headers, config) {
+			pooloptions.vote++;
+		});
 	}
-   
+
+});
+app.controller('adminController', function($scope, $http) {
+
+	$scope.pools = [];
+	$scope.newpool = { };
+	$http.get('/api/pool').
+	success(function(data, status, headers, config) {
+		$scope.pools = data;
+	});
+	$scope.addPool = function() {
+		$http.post('/api/pool', $scope.newpool).
+		success(function(data, status, headers, config) {
+			data.pooloptions=[];
+			$scope.pools.push(data);
+			$scope.newpool.title="";
+		});
+	}
+	$scope.removePool = function(pool) {
+		$http.delete('/api/pool/' + pool.id).
+		success(function(data, status, headers, config) {
+			for (index = 0; index < $scope.pools.length; ++index) {
+				if ($scope.pools[index].id == pool.id) {
+					$scope.pools.splice(index, 1);
+				}
+			}
+		});
+	}
+	$scope.addOption = function(pool,newoption) {
+		newoption.pool_id=pool.id;
+		$http.post('/api/pooloption', newoption).
+		success(function(data, status, headers, config) {
+			pool.pooloptions.push(data);
+			newoption.title='';
+		});	
+	}
+	$scope.removeOption = function(pooloptions, pool) {
+		$http.delete('/api/pooloption/' + pooloptions.id).
+		success(function(data, status, headers, config) {
+			for (index = 0; index < pool.pooloptions.length; ++index) {
+				if (pool.pooloptions[index].id == pooloptions.id) {
+					pool.pooloptions.splice(index, 1);
+				}
+			}
+
+		});
+	}
 });
